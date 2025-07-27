9cb4e5b99f9d78e66d9861d8fa8d7ffee00c1dc2
CHUNG HONG MING <euguien0302@gmail.com>
2025-07-27 22:18:46 +0800

    Fix build and dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
+// vite.config.js
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: './', // Use './' if you're deploying to a subdirectory or static host
-  plugins: [react()],
+  base: './', // Use relative base path to fix blank page issue
 })
